3147eda0639f5d996c5236429122a9b6ec81d416
imane chater <imane.chater30@gmail.com>
2023-10-28 06:48:58 -0700

    fix button

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -67,7 +67,7 @@ export default function FormSignUp() {
           type="submit"
           className="flex w-full justify-center rounded-md border border-transparent bg-sky-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
         >
-          Sign in
+          Register
         </button>
       </div>
       <div>
